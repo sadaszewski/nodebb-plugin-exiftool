ee2eae43c96328a658c036cc5593a59c7ded79d7
sadaszewski <s.adaszewski@gmail.com>
2022-04-01 13:58:46 +0200

    Update library.js

--- library.js
+++ library.js
@@ -7,7 +7,7 @@ const plugin = {};
 
 plugin.stripEXIF = async function(data) {
 	const { path } = data;
-	await execFile('exiftool', ['-all=', '-tagsfromfile', '@', '-Orientation', path]);
+	await execFile('exiftool', [ '-all=', '-tagsfromfile', '@', '-Orientation', path ]);
 };
 
 module.exports = plugin;
