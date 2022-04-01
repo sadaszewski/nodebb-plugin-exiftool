bae1ece01d2d95cbc3ee2ee9c5bef68cfd9d3d14
sadaszewski <s.adaszewski@gmail.com>
2022-04-01 13:58:29 +0200

    Update library.js

--- library.js
+++ library.js
@@ -5,7 +5,8 @@ const execFile = util.promisify(require('child_process').execFile);
 
 const plugin = {};
 
-plugin.stripEXIF = async function(path) {
+plugin.stripEXIF = async function(data) {
+	const { path } = data;
 	await execFile('exiftool', ['-all=', '-tagsfromfile', '@', '-Orientation', path]);
 };
 
